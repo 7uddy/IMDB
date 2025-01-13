478b3eb0446f6935a5ccc3535f5da28fa55a79bc
Ene Călin Tudor <65676750+7uddy@users.noreply.github.com>
2025-01-13 20:36:31 +0200

    Refactor header component to implement a responsive navigation bar with login/logout functionality and improve styling.

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { Emitters } from '../emitters/emitters';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
 
   authenticated: boolean = false;
-  constructor(private router: Router,private auth:AuthService) { }
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit(): void {
     Emitters.authEmitter.subscribe((auth: boolean) => {
@@ -24,8 +26,19 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/home']);
   }
   logout() {
-    this.auth.logout().subscribe(() => {
-      this.authenticated = false;
+    this.auth.logout().subscribe({
+      next: (data) => {
+        this.authenticated = false;
+      },
+      error: (error) => {
+        if (error.error && error.error.error) {
+          console.error(error.error);
+        }
+      }
     });
+  }
+
+  goTo(route: string): void {
+    this.router.navigate([`/${route}`]);
   }
 }
